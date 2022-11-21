5599ec667e426bdf139fddd7602dec3f11603fb8
htack210 <glenn.hline@outlook.com>
2022-11-21 11:15:01 -0500

    Fun with conversions.

--- src/com/java/udemy/Main.java
+++ src/com/java/udemy/Main.java
@@ -3,5 +3,14 @@ package com.java.udemy;
 public class Main {
     public static void main(String[] args) {
     //67 keywords in Java
+        // A mile is equal to 1.609344 kilometers
+        double km = 1.609344;
+        double numMiles = 5.0;
+        double numKm = 5.0;
+        double convertedMile = numMiles * km;
+
+        System.out.println(numMiles + " mile(s) = " + convertedMile);
+        System.out.println(numKm + " km = " + numKm/km + " miles.");
+
     }
 }
